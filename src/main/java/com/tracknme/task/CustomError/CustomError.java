8baa5c2c1c289b94656022121a895432caaac6ac
HugoTiyoda <hugotiyoda@gmail.com>
2022-02-03 23:23:47 -0300

    att1

---
+++ src/main/java/com/tracknme/task/CustomError/CustomError.java
@@ -0,0 +1,14 @@
+package com.tracknme.task.CustomError;
+
+public class CustomError {
+
+    private final String errorMessage;
+
+    public CustomError(String errorMessage) {
+        this.errorMessage = errorMessage;
+    }
+
+    public String getErrorMessage() {
+        return errorMessage;
+    }
+}
